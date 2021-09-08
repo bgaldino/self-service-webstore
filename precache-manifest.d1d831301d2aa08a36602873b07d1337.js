55657b4e1ffda9b10d992dd0c4bc38aceeabebe9
Brian Galdino <bgaldino@salesforce.com>
2021-09-08 13:10:08 -0700

    Updates

---
+++ precache-manifest.d1d831301d2aa08a36602873b07d1337.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc01aa85db8c7262766a106390d5a853",
+    "url": "/self-service-webstore/index.html"
+  },
+  {
+    "revision": "3e133784165e71c49325",
+    "url": "/self-service-webstore/static/css/2.058fa3a9.chunk.css"
+  },
+  {
+    "revision": "38074b185cd920db5233",
+    "url": "/self-service-webstore/static/css/main.2074fed2.chunk.css"
+  },
+  {
+    "revision": "3e133784165e71c49325",
+    "url": "/self-service-webstore/static/js/2.397eab72.chunk.js"
+  },
+  {
+    "revision": "74fcb650c437eba4a2d597e834f687cd",
+    "url": "/self-service-webstore/static/js/2.397eab72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38074b185cd920db5233",
+    "url": "/self-service-webstore/static/js/main.9fce7918.chunk.js"
+  },
+  {
+    "revision": "3d2b8b348feee0ae7cc9",
+    "url": "/self-service-webstore/static/js/runtime-main.ed55b19c.js"
+  },
+  {
+    "revision": "57476e8f60c7521c552782c46f616876",
+    "url": "/self-service-webstore/static/media/amex.57476e8f.png"
+  },
+  {
+    "revision": "852b0f1760d47eaff7b07a1ee6f7586c",
+    "url": "/self-service-webstore/static/media/demoAnalytics.852b0f17.png"
+  },
+  {
+    "revision": "fa6db1cff3f46f06a079af6543519eba",
+    "url": "/self-service-webstore/static/media/diners.fa6db1cf.png"
+  },
+  {
+    "revision": "2e11480db78db01226b802e8253d30d4",
+    "url": "/self-service-webstore/static/media/jcb.2e11480d.png"
+  },
+  {
+    "revision": "da2b63ddaebc620efa13c39f39ed8ee8",
+    "url": "/self-service-webstore/static/media/loading.da2b63dd.gif"
+  },
+  {
+    "revision": "c59f526c377f93480aa1be9f6640fc8d",
+    "url": "/self-service-webstore/static/media/maestro.c59f526c.png"
+  },
+  {
+    "revision": "1c3adf6310467e852051627eaaef31ea",
+    "url": "/self-service-webstore/static/media/owned.1c3adf63.png"
+  },
+  {
+    "revision": "e66d38d0e8d470d71f3422450b256ebc",
+    "url": "/self-service-webstore/static/media/visa.e66d38d0.jpg"
+  }
+]);
