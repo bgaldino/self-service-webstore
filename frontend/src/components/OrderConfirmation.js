63908e8eafe80a33f3aafc72d6229580ad4b8895
Brian Galdino <bgaldino@salesforce.com>
2021-09-16 12:37:11 -0700

    more styles

--- frontend/src/components/OrderConfirmation.js
+++ frontend/src/components/OrderConfirmation.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useToken from "./useToken";
+import smartbytes from "../images/smartbytes.png"
 
 export default function OrderConfirmation() {
   const { orderId } = useParams();
@@ -9,6 +10,7 @@ export default function OrderConfirmation() {
   return (
     <div className="container">
       <div className="order-header">
+      <img className="header-image" src={smartbytes}/>
         <div style={{ 'padding-left': '1em' }}>
           <h4>Order Confirmation #{orderId}</h4>
           <h5>Thank you for your order!</h5>
